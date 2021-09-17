f7e82a897b95ec6a21732e1b443d897fd16617d1
Vu Quynh Nga <nina@Ngas-MacBook-Pro.local>
2021-09-17 14:54:19 -0400

    Final attempt

--- product-digit-sum.cpp
+++ product-digit-sum.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-unsigned long long pow(int a, int b){
-	unsigned long long result = 1; 
+unsigned long long pow(unsigned int a, unsigned int b){		// function calculates a^b
+	unsigned long long result = 1;		
 	
 	for (int i = 0; i < b; i++){
 		result *= a;
@@ -16,7 +16,7 @@ int main(){
 	int a;
 	int b;
 	unsigned long long product;
-	int sum = 0;
+	int sumDigit = 0;
 	
 	cout << "a: ";
 	cin >> a;
@@ -24,23 +24,24 @@ int main(){
 	cout << "b: ";
 	cin >> b;
 	
-	if (a >= 0 && b >= 0){				//only calculate if the input is valid
-		product = pow(a, b);			//call pow(a,b) function to calculate a^b
+	if (a >= 0 && b >= 0){				// only calculate if the input is valid
+		product = pow(a, b);			// call pow(a,b) function to calculate a^b
 	
 		cout << a << "^" << b << " = " << product << endl;
 	
 		//calculate the sum of the digits of the product
 		while (product > 0){
-			
-			sum += product % 10;	//add the last digit of the number to sum			
-			product /= 10;			//remove last digit directly from product to calculate a new last digit			
+			sumDigit += product % 10;	// add the last digit of the number to sum			
+			product /= 10;				// remove last digit directly from product to calculate a new last digit			
 		}
 		
-		cout << "Sum Of Digits: " << sum << endl;
+		cout << "Sum Of Digits: " << sumDigit << endl;
 		
 	} else {
-		cout << "Invalid input! You dumb dumb!" << endl;
+		cout << "Invalid input!" << endl;
 	}
 	return 0;
 
 }
+
+// 0 times per day on average this thought has occured to me last week
